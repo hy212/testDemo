9aaa3e944747ab54a7d17424ef8ccea0bb5e3fa3
xyinghu <xyinghu@tencent.com>
2025-07-25 18:32:10 +0800

    server timing api测试

---
+++ servertime.js
@@ -0,0 +1,24 @@
+/**
+ * 主请求监听器
+ * 监听fetch事件并处理所有传入请求
+ * @param {FetchEvent} event - fetch事件对象
+ */
+addEventListener("fetch", async (event) => {
+  const res = await handleRequest(event.request);
+  event.respondWith(res);
+});
+
+/**
+ * 请求处理主函数
+ * 处理HTTP请求并返回响应
+ * @param {Request} req - 传入的HTTP请求对象
+ * @returns {Response} 处理后的HTTP响应
+ */
+async function handleRequest(req) {
+  return new Response("111", {
+    status: 200,
+    headers: {
+      "Server-Timing": 'fetch;desc="fetch请求耗时";dur=300, compute;dur=100',
+    },
+  });
+}
