8a4edba3dcd2cab3138f4b985728a45fa13de83c
xyinghu <xyinghu@tencent.com>
2025-07-25 15:01:07 +0800

    测试响应体为空的情况

---
+++ functions/testbody.js
@@ -0,0 +1,19 @@
+/**
+ * 主请求监听器
+ * 监听fetch事件并处理所有传入请求
+ * @param {FetchEvent} event - fetch事件对象
+ */
+addEventListener("fetch", async (event) => {
+  const res = await handleRequest(event.request);
+  event.respondWith(res);
+});
+
+/**
+ * 请求处理主函数
+ * 处理HTTP请求并返回响应
+ * @param {Request} req - 传入的HTTP请求对象
+ * @returns {Response} 处理后的HTTP响应
+ */
+async function handleRequest(req) {
+  return new Response(null, { status: 500 });
+}
