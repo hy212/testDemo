875382dc365db235e132802392a6ee4a2157511a
xyinghu <xyinghu@tencent.com>
2025-10-11 19:29:42 +0800

    update

--- functions/admin.js
+++ functions/admin.js
@@ -1,4 +1,22 @@
 /**
+ * 管理员接口说明：
+ * 1、更新KV： /admin/$USRENAME/kv/$NAMESPACE/update
+ * 请求参数：
+ *  {
+ *   "key": "test",
+ *   "value": "test"
+ *  }
+ *
+ * 2、删除KV: /admin/$USRENAME/kv/$NAMESPACE/delete
+ * 请求参数：
+ *  {
+ *   "key": "test"
+ *  }
+ */
+
+const API_ACTIONS = ["update", "delete"];
+
+/**
  * 主请求监听器
  * 监听fetch事件并处理所有传入请求
  * @param {FetchEvent} event - fetch事件对象
@@ -23,20 +41,31 @@ async function handleRequest(req) {
         status: 405,
       });
     }
+
+    const apiUrl = new URL(req.url).pathname;
+    const paramsArr = apiUrl.split("/").slice(1);
+    if (paramsArr.length !== 6) {
+      return new Response("请检查接口路径是否正确", {
+        status: 400,
+      });
+    }
+    const params = {
+      username: paramsArr[1],
+      kvNamespace: paramsArr[3],
+      action: paramsArr[4], // update & delete
+    };
+    if (!API_ACTIONS.includes(params.action)) {
+      return new Response("请检查接口路径是否正确", {
+        status: 400,
+      });
+    }
+
     const reqArgs = await req.json();
     if (!reqArgs.key) {
       return new Response("key不能为空", {
         status: 400,
       });
     }
-    const apiUrl = new URL(req.url).pathname;
-    const paramsArr = apiUrl.split("/").slice(1);
-
-    const params = {
-      username: paramsArr[2],
-      kvNamespace: paramsArr[4],
-      action: paramsArr[5], // update & delete
-    };
 
     // TODO 鉴权逻辑
 
@@ -49,6 +78,9 @@ async function handleRequest(req) {
       await KV.delete(reqArgs.key);
       return new Response(`删除成功，key: ${reqArgs.key}`);
     }
+    return new Response(`暂不支持${params.action}操作`, {
+      status: 400,
+    });
   } catch (e) {
     const msgData = {
       name: e.name,
