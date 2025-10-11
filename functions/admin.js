8c93ae40f246deed6a9d64d269b395a7afe31062
xyinghu <xyinghu@tencent.com>
2025-10-11 18:51:36 +0800

    update

--- functions/admin.js
+++ functions/admin.js
@@ -4,8 +4,8 @@
  * @param {FetchEvent} event - fetch事件对象
  */
 addEventListener("fetch", async (event) => {
-    const res = await handleRequest(event.request);
-    event.respondWith(res);
+  const res = await handleRequest(event.request);
+  event.respondWith(res);
 });
 
 /**
@@ -15,39 +15,44 @@ addEventListener("fetch", async (event) => {
  * @returns {Response} 处理后的HTTP响应
  */
 async function handleRequest(req) {
-    try {
-        const { method } = req;
-        if (method !== "POST") {
-            // 暂不支持${method}请求，请使用POST请求
-            return new Response(null, {
-                status: 405,
-            });
-        }
-        const apiUrl = new URL(req.url).pathname;
-        const reqArgs = await req.json();
-
-        const paramsArr = apiUrl.split('/').slice(1);
-
-        const params = {
-            username: paramsArr[2],
-            kvNamespace: paramsArr[4],
-            action: paramsArr[5], // update & delete
-        }
-
-        // TODO 鉴权逻辑
-
-        if (params.action === "update") {
-            await KV.put(reqArgs.key, reqArgs.value);
-        } else if (params.action === "delete") {
-            await KV.delete(reqArgs.key);
-        }
-
-        return new Response(`操作成功`);
-    } catch (e) {
-        const msgData = {
-            name: e.name,
-            message: e.message,
-        };
-        return new Response(JSON.stringify(msgData), { status: 500 });
+  try {
+    const { method } = req;
+    if (method !== "POST") {
+      // 暂不支持${method}请求，请使用POST请求
+      return new Response(null, {
+        status: 405,
+      });
     }
+    const reqArgs = await req.json();
+
+    if (!reqArgs.key?.trim()) {
+      return new Response("key不能为空", {
+        status: 400,
+      });
+    }
+    const apiUrl = new URL(req.url).pathname;
+    const paramsArr = apiUrl.split("/").slice(1);
+
+    const params = {
+      username: paramsArr[2],
+      kvNamespace: paramsArr[4],
+      action: paramsArr[5], // update & delete
+    };
+
+    // TODO 鉴权逻辑
+
+    if (params.action === "update") {
+      await KV.put(reqArgs.key, reqArgs.value);
+    } else if (params.action === "delete") {
+      await KV.delete(reqArgs.key);
+    }
+
+    return new Response(`操作成功, ${JSON.stringify(reqArgs)}`);
+  } catch (e) {
+    const msgData = {
+      name: e.name,
+      message: e.message,
+    };
+    return new Response(JSON.stringify(msgData), { status: 500 });
+  }
 }
