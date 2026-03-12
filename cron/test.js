e7591cc188974276a717c1e16c86df307cc14eda
xyinghu <xyinghu@tencent.com>
2026-03-12 15:28:33 +0800

    test cron

---
+++ cron/test.js
@@ -0,0 +1,25 @@
+/** 错误日志上报智研 **/
+export function reportLog() {
+  const queryArgs = {
+    topic: "sdk-cce19ccdcee273db",
+    host: "cron-test",
+    data: [
+      {
+        timestamp: Date.now(),
+        message: "定时任务执行成功",
+        fields: {
+          url: "/cron/test",
+        },
+      },
+    ],
+  };
+  fetch(`https://publiclog.zhiyan.tencent-cloud.net/collect`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(queryArgs),
+  }).then((res) => res.json());
+}
+
+reportLog();
