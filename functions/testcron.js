9d5e7a1a817d661faccefc1c7e0a9bf2c84a6321
xyinghu <xyinghu@tencent.com>
2026-03-12 16:40:41 +0800

    test cron

--- functions/testcron.js
+++ functions/testcron.js
@@ -1,34 +1,33 @@
 addEventListener("fetch", async (event) => {
-  try {
-    reportLog();
-    event.respondWith(new Response("访问成功"));
-  } catch (e) {
-    event.respondWith(new Response("请求reportLog异常"));
-  }
+  const res = await reportLog();
+  event.respondWith(res);
 });
 
 /** 错误日志上报智研 **/
-function reportLog() {
-  const queryArgs = {
-    topic: "sdk-cce19ccdcee273db",
-    host: "cron-test",
-    data: [
-      {
-        timestamp: Date.now(),
-        message: "定时任务执行成功",
-        fields: {
-          url: "/cron/test",
+async function reportLog() {
+  let isSuccess = false;
+  try {
+    const queryArgs = {
+      topic: "sdk-cce19ccdcee273db",
+      host: "cron-test-function",
+      data: [
+        {
+          timestamp: Date.now(),
+          message: "执行成功",
+          fields: {
+            url: "/cron/test",
+          },
         },
+      ],
+    };
+    await fetch(`https://publiclog.zhiyan.tencent-cloud.net/collect`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
       },
-    ],
-  };
-  fetch(`https://publiclog.zhiyan.tencent-cloud.net/collect`, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(queryArgs),
-  }).then((r) => r.json());
+      body: JSON.stringify(queryArgs),
+    });
+    isSuccess = true;
+  } catch (e) {}
+  return new Response(isSuccess ? "访问成功" : "访问失败");
 }
-
-// reportLog();
